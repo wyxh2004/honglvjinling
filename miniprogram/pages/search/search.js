d8aab4610f9e080549b58dcf0fc1faf7d47f16af
WYXH <103009253+WeiYuXingHan@users.noreply.github.com>
2024-01-28 18:26:53 +0800

    20240128-2

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,4 +1,9 @@
 Page({
+    data: {
+        inputValue: '',
+        searchHistory: [], // 搜索历史数据
+        guessWhatToSearch: ['文学客厅', '红楼梦', '玄武湖', '栖霞山', '六朝', '设计廊', '王安石', '秦淮', '民国建筑', '城墙', '美术馆', '李白', '儒林外史', '乌衣巷', '鸡鸣寺', '朱元璋', '雨花台', '谢灵运', '金陵', '文心雕龙', '颜真卿', '南京博物院', '夫子庙', '钟山风景名胜区'], // 猜你想搜数据
+    },
     onShareAppMessage: function () {
         return {
             title: '分享标题',
@@ -34,10 +39,5 @@ Page({
         wx.navigateTo({
             url: '/pages/location/location?inputValue=' + encodeURIComponent(inputValue),
         });
-    },
-    data: {
-        inputValue: '',
-        searchHistory: [], // 搜索历史数据
-        guessWhatToSearch: ['文学客厅', '红楼梦', '玄武湖', '栖霞山', '六朝', '设计廊', '王安石', '秦淮', '民国建筑', '城墙', '美术馆', '李白', '儒林外史', '乌衣巷', '鸡鸣寺', '朱元璋', '雨花台', '谢灵运', '金陵', '文心雕龙', '颜真卿', '南京博物院', '夫子庙', '钟山风景名胜区'], // 猜你想搜数据
     },
 })
