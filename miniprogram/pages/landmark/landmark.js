471246b337afe2b22b3e9f34d24a8962e8216478
wyxh2004 <103009253+WeiYuXingHan@users.noreply.github.com>
2024-03-03 22:35:45 +0800

    cloudfunction and datas

--- miniprogram/pages/landmark/landmark.js
+++ miniprogram/pages/landmark/landmark.js
@@ -2,44 +2,24 @@ Page({
   data: {
     landmarkList: [
       {
-        name: "侵华日军南京大屠杀纪念馆",
+        name: "孙津川烈士墓",
         imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
-      },
-      {
-        name: "南京抗日航空烈士纪念馆",
-        imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
-      },
-      {
-        name: "中山陵",
-        imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
+          "https://p.ananas.chaoxing.com/star3/origin/5dc8e51aab1e31d0158e77104926d208.png",
       },
       {
         name: "雨花台烈士陵园",
         imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
+          "https://www.rednanjing.cn/upload/adminredsource/20191016/8108eff071cf5f907404c18c8a529b41.jpg",
       },
       {
-        name: "渡江胜利纪念馆",
+        name: "南京抗日航空烈士纪念馆",
         imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
+          "https://p3-sdbk2-media.byteimg.com/tos-cn-i-xv4ileqgde/d04e698a692248a680adb5f57229a1b7~tplv-xv4ileqgde-resize-w:750.image",
       },
       {
         name: "南京民间抗日战争博物馆",
         imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
-      },
-      {
-        name: "桂子山烈士陵园",
-        imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
-      },
-      {
-        name: "大金山国防园",
-        imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
+          "https://mzt.jiangsu.gov.cn/picture/0/s_5536cb0133594837b83a7207f77ff0d3.jpg",
       },
       {
         name: "云台山抗日烈士陵园",
@@ -47,14 +27,23 @@ Page({
           "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
       },
       {
-        name: "中华民国革命史博物馆",
+        name: "中国代表团，梅园新村",
         imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
+          "https://www.rednanjing.cn/upload/adminredsource/20190917/9604b95f391cb67f7257088e076fa2c5.jpg",
       },
       {
-        name: "南京工运纪念馆",
+        name: "渡江胜利纪念馆",
         imgUrl:
-          "https://bbs.scumaker.org/uploads/default/original/1X/ddb7c3bbf822a8aea931470a41797d1ecd0e6ac1.png",
+          "https://www.rednanjing.cn/upload/adminredsource/20190923/a1b45fb29ca01af50035a794a58866e0.jpg",
+      },
+      {
+        name: "颐和路社区将军馆",
+        imgUrl: "https://dimg04.c-ctrip.com/images/0103n120008g2tgn5FF20.jpg",
+      },
+      {
+        name: "江北新区红色广场",
+        imgUrl:
+          "https://dimg04.c-ctrip.com/images/1me7112000cnyixfbFF45_R_800_10000_Q90.jpg?proc=source/tripcommunity",
       },
     ],
   },
@@ -63,13 +52,4 @@ Page({
       url: "/pages/subpages/search/search",
     });
   },
-  // onLoad: function (options) {
-  //     var { landmark } = options;
-  //     var landmark_item = this.data.data.filter(item => item.name === landmark)[0];
-  //     this.setData({
-  //         ...this.data,
-  //         data: landmark_item
-  //     })
-  //     console.log(this.data.data)
-  // },
 });
