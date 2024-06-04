f9a5a48afa7459d997a0ef5b6e775ccca26420b6
wyxh2004 <103009253+WeiYuXingHan@users.noreply.github.com>
2024-06-05 01:05:25 +0800

    Clearing git cache

--- uploadCloudFunction.bat
+++
@@ -1,3 +0,0 @@
-"D:\wx_web_devtool\cli.bat" cloud functions deploy --e honglvjinling-0g7519ot1e8f297c --n quickstartFunctions --r --project "D:\git\LandmarkMap\cloudfunctions\quickstartFunctions" --report_first --report
-
-
